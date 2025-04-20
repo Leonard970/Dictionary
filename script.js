ff7fb174331ecb05a763a21e8685f1ebf14c3f4a
Leonard970 <leonardnaron6@gmail.com>
2025-04-20 22:17:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function isDesktopOrTabletButNotAndroid() {
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  const isAndroid = /Android/i.test(userAgent);
+  const isMobile = /Mobi|Phone/i.test(userAgent);
+  return !isAndroid && !isMobile;
+}
+
+if (isDesktopOrTabletButNotAndroid()) {
+  document.body.style.display = "none";
+  location.reload();
+  alert("Desktop Detected!")
+} else {
+  console.log("Device is Android or mobile.");
+};
+function login(){
+  const inputPassword = document.getElementById("inpt1").value;
+  const password = "Admin@leonard25";
+  if (inputPassword === password) {
+    window.location.href = "index2.html";
+  } else {
+    alert("Password Is not Correct!!")
+  }
+}
